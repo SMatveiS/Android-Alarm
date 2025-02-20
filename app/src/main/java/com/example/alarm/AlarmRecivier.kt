f2d35569bc7b947800a903a8ac881cd700800432
SMatveiS <matvei2235@gmail.com>
2025-02-21 00:39:25 +0300

    "Aaa"

--- app/src/main/java/com/example/alarm/AlarmRecivier.kt
+++ app/src/main/java/com/example/alarm/AlarmRecivier.kt
@@ -44,6 +44,8 @@ class AlarmReceiver: BroadcastReceiver() {
                 Manifest.permission.POST_NOTIFICATIONS
             ) != PackageManager.PERMISSION_GRANTED
         ) {
+            print("hi")
+            print("io")
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
