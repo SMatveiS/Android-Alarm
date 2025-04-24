bc539fe7da0c4ecec667c0f6e96feedf329ec02e
SMatveiS <matvei2235@gmail.com>
2025-04-24 14:45:48 +0300

    delete unused import

--- app/src/main/java/com/example/alarm/BuildAlarmsFragment.kt
+++ app/src/main/java/com/example/alarm/BuildAlarmsFragment.kt
@@ -17,7 +17,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.CompoundButton
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
